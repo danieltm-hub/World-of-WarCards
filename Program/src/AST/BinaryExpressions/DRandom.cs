fa782733e11c07793542ad280933b596b6549ae7
val46 <osvaldo020213@gmail.com>
2022-12-28 02:20:41 +0100

    "" commit

---
+++ Program/src/AST/BinaryExpressions/DRandom.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AST
+{
+    public class DRandom : BinaryExpression
+    {
+        public override Func<Expression, Expression, bool> IsValid => (left, right) => left.Type == NodeType.Number && right.Type == NodeType.Number;
+        public override NodeType Type { get; set; }
+        public override string OperationSymbol => "+";
+        public override object Value { get; set; }
+        public DRandom(Expression left, Expression right, CodeLocation location) : base(left, right, location)
+        {
+            Type = NodeType.Number;
+            Value = 0;
+        }
+        public override void Evaluate()
+        {
+            base.Evaluate();
+            double right = (double)Right.Value;
+            double left = (double)Left.Value;
+            Random random = new Random();
+
+            Value = random.NextDouble() % (right - left) + left;
+        }
+    }
+}
