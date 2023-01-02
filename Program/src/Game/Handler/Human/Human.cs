ac0b9508f00f711595218c232b8c2db47c94afa6
val46 <osvaldo020213@gmail.com>
2023-01-02 03:08:50 +0100

    Human Skeleton

---
+++ Program/src/Game/Handler/Human/Human.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using AST;
+
+namespace GameProgram
+{
+    public class Human : Handler
+    {
+        public Human(Player player) : base(player) { }
+        public override HashSet<string> GetCards()
+        {
+            Game initialGame = GameManager.CurrentGame;
+           
+            GameManager.CurrentGame = initialGame.Clone();
+
+            HashSet<string> toPlay = HumanController();
+
+            GameManager.CurrentGame = initialGame;
+
+            return toPlay;
+
+        }
+
+        public HashSet<string> HumanController()
+        {
+            HashSet<string> cardtoPlay = new HashSet<string>();
+
+            
+            return cardtoPlay;
+        }
+
+        
+    }
+
+}
